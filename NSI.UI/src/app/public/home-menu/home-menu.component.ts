d16f6f2b5295476e86d39ca646319d2d4f986803
ahrustic13 <ahrustic13@gmail.com>
2021-11-19 22:34:47 +0100

    login and logout component

--- NSI.UI/src/app/public/home-menu/home-menu.component.ts
+++ NSI.UI/src/app/public/home-menu/home-menu.component.ts
@@ -1,5 +1,5 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {MSAL_GUARD_CONFIG, MsalBroadcastService, MsalGuardConfiguration, MsalService} from "@azure/msal-angular";
+import {MSAL_GUARD_CONFIG, MsalBroadcastService, MsalGuardConfiguration, MsalService} from '@azure/msal-angular';
 
 @Component({
   selector: 'app-home-menu',
@@ -8,18 +8,27 @@ import {MSAL_GUARD_CONFIG, MsalBroadcastService, MsalGuardConfiguration, MsalSer
 })
 export class HomeMenuComponent implements OnInit {
 
+  isIframe = false;
+
   constructor(@Inject(MSAL_GUARD_CONFIG) private msalGuardConfig: MsalGuardConfiguration,
               private broadcastService: MsalBroadcastService,
               private authService: MsalService) {
+    this.isIframe = window !== window.parent && !window.opener;
   }
 
   ngOnInit(): void {
+  }
+
+  login(): void {
+    this.authService.loginRedirect();
+    this.isIframe = true;
   }
 
   logout(): void {
     this.authService.logoutRedirect({
       postLogoutRedirectUri: 'http://localhost:4200'
     });
+    this.isIframe = false;
   }
 
 }
