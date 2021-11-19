6ce89b837283f27b5e053c37b0bb83375916f555
ahrustic13 <ahrustic13@gmail.com>
2021-11-19 22:57:46 +0100

    login and logout fix

--- NSI.UI/src/app/public/home-menu/home-menu.component.ts
+++ NSI.UI/src/app/public/home-menu/home-menu.component.ts
@@ -1,5 +1,8 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MSAL_GUARD_CONFIG, MsalBroadcastService, MsalGuardConfiguration, MsalService} from '@azure/msal-angular';
+import {InteractionStatus} from '@azure/msal-browser';
+import {filter, takeUntil} from 'rxjs/operators';
+import {Subject} from 'rxjs';
 
 @Component({
   selector: 'app-home-menu',
@@ -9,19 +12,40 @@ import {MSAL_GUARD_CONFIG, MsalBroadcastService, MsalGuardConfiguration, MsalSer
 export class HomeMenuComponent implements OnInit {
 
   isIframe = false;
+  loginDisplay = false;
+  readonly #_destroying$ = new Subject<void>();
 
   constructor(@Inject(MSAL_GUARD_CONFIG) private msalGuardConfig: MsalGuardConfiguration,
               private broadcastService: MsalBroadcastService,
               private authService: MsalService) {
-    this.isIframe = window !== window.parent && !window.opener;
   }
 
   ngOnInit(): void {
+    this.isIframe = window !== window.parent && !window.opener;
+
+    this.broadcastService.inProgress$
+      .pipe(
+        filter((status: InteractionStatus) => status === InteractionStatus.None),
+        takeUntil(this.#_destroying$)
+      )
+      .subscribe(() => {
+        this.setLoginDisplay();
+      });
   }
 
   login(): void {
-    this.authService.loginRedirect();
-    this.isIframe = true;
+    this.authService.loginRedirect().subscribe(
+      {
+        next: (result) => {
+          this.setLoginDisplay();
+        },
+        error: (error) => console.log(error)
+      }
+    );
+  }
+
+  setLoginDisplay(): void {
+    this.loginDisplay = this.authService.instance.getAllAccounts().length > 0;
   }
 
   logout(): void {
