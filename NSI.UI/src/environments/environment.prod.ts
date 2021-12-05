c7c61c376057d7e78292a9b5ba60b6a4a085b08a
epita1 <epita1@etf.unsa.ba>
2021-12-05 21:34:31 +0100

    reverted back

--- NSI.UI/src/environments/environment.prod.ts
+++ NSI.UI/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url: 'https://nsi-dev-api-echo.azurewebsites.net:44318'
+  url: 'https://localhost:44318'//'https://nsi-dev-api-echo.azurewebsites.net:44318'
 };
