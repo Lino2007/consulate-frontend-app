ebc5a1d2acb83ae76fc88af4a93881c0fb62dd12
epita1 <epita1@etf.unsa.ba>
2021-12-07 18:42:56 +0100

    Reverted to localhost

--- NSI.UI/src/environments/environment.ts
+++ NSI.UI/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   newsSource: 'https://newsapi.org/v2/top-headlines?country=us&apiKey=1fba2bc8d1b9461bbc31d2fc0efc6962',
   clientId: 'f18b4dc4-a2c6-4df1-97d4-8e24bcc1a342',
   loginRedirectUri: 'https://login.microsoftonline.com/40d05a0d-8aeb-415d-96a1-8722f450a418',
-  appRedirectUri: 'https://nsi-dev-api-echo.azurewebsites.net/',//'http://localhost:4200',//
+  appRedirectUri: 'http://localhost:4200',//'https://nsi-dev-api-echo.azurewebsites.net/',
   protected: [
     ["https://graph.microsoft.com/v1.0/me","user.read"],
     ["https://graph.microsoft.com/User.ReadBasic.All", "user.readbasic.all"],
